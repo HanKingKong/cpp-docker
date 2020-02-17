5da297a5ee54ec01492edbce15e6451e7342b6cf
HanKingKOng <1328968413@qq.com>
2020-02-17 12:47:57 +0800

    first commit

--- docker.hpp
+++ docker.hpp
@@ -1,2 +1,18 @@
 #include <sys.wait.h>	// waitpid
 #include <sys/mount.h>  // mount
+#include <fcntl.h>      // open
+#include <unistd.h>     // execv, sethostname, chroot, fchdir
+#include <sched.h>
+
+#include <cstring>  // C   lib
+#include <string>   // C++ lib
+
+#define STACK_SIZE (512*512)
+
+namespace docker {
+    typedef int proc_statu;
+
+
+
+}
+
